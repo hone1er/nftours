06dfdd9e866f0cf46feb10eff51ca2c9381656b7
Joe <josephmvillavicencio@gmail.com>
2021-10-30 11:57:03 -0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">NFTours</p>
+        <Link href="/">
+          <a className="mr-4 text-purple-500">Home</a>
+        </Link>
+        <Link href="/discover">
+          <a className="mr-4 text-purple-500">Discover</a>
+        </Link>
+        <Link href="/gallery">
+          <a className="mr-4 text-purple-500">My NFTs</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
