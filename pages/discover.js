6a34955d02b758967a04313ba33a008c254e0af3
Joe <josephmvillavicencio@gmail.com>
2021-11-01 13:13:15 -0700

    remove prebuild

--- pages/discover.js
+++ pages/discover.js
@@ -82,12 +82,7 @@ export default function Discover() {
         Get Coordinates
       </button>
       <button
-        disabled={!distance < 4}
-        className={
-          distance < 4
-            ? "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            : "bg-blue-500 text-white font-bold py-2 px-4 rounded-full"
-        }
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         onClick={handleMint}
       >
         Mint NFT
