8ceae02980b8d3c4b785d0d5192507dd05a8e8ca
Joe <josephmvillavicencio@gmail.com>
2021-11-14 18:12:38 -0800

    subgraph

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 // 0xd7BEaA6D7BD084cCEb1112b996e18f3d8b266dd0 contract address
 
 contract NFTour is ERC721URIStorage {
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AppContext from "../AppContext";
 import styles from "../styles/Home.module.css";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { login } from "../scripts/login";
+
 export default function Gallery() {
+  useEffect(() => {
+    async function balances() {
+      const web3Modal = new Web3Modal({
+        network: "ropsten",
+        cacheProvider: true,
+      });
+
+      const contract = require("/scripts/NFTour.json");
+      const contractAddress = "0xd7beaa6d7bd084cceb1112b996e18f3d8b266dd0";
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress()
+      const nftContract = new ethers.Contract(
+        contractAddress,
+        contract.abi,
+        signer
+      );
+      console.log(await nftContract.functions.balanceOf(address));
+    }
+    balances();
+  }, []);
   const { signed, setSigned } = useContext(AppContext);
   async function handleLogin() {
     setSigned(await login());
