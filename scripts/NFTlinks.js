116792e8a00437395757ed763f117932dc1f0086
Joe <josephmvillavicencio@gmail.com>
2021-12-08 11:37:58 -0800

    added all of Jill's photos

--- scripts/NFTlinks.js
+++ scripts/NFTlinks.js
@@ -1,68 +1,86 @@
 export const NFTlinks = [
   {
-    city: "Hercules",
-    country: "United States",
-    location: [38.0171441, -122.2885808],
+    city: "Uxmal",
+    country: "Mexico",
+    location: [20.359444, -89.771389],
     tokenURI:
-      "https://gateway.pinata.cloud/ipfs/QmdxYSVWofjT2hZQypGBB16bVLp2hiQhxLrmSXNqXmegsb/hercules.json",
-    description: "Hercules, CA",
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/uxmal.json",
+    description: "Uxmal, Mexico",
     image:
-      "https://gateway.pinata.cloud/ipfs/QmecvVa4BRgzMCvY5jDFPiRmuYcqpnwTH6TKSLhznjoZDg",
-    name: "Dynamite Factory",
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/uxmal.JPG",
+    name: "Ancient Maya City",
+    photographer: "Jill Schweber",
   },
   {
-    city: "Madrid",
-    country: "Spain",
-    location: [40.416775, -3.70379],
+    city: "Tulum",
+    country: "Mexico",
+    location: [20.214722, -87.428889],
     tokenURI:
-      "https://gateway.pinata.cloud/ipfs/QmdxYSVWofjT2hZQypGBB16bVLp2hiQhxLrmSXNqXmegsb/madrid.json",
-    description: "Madrid, Spain",
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/tulum.json",
+    description: "Tulum, Mexico",
     image:
-      "https://gateway.pinata.cloud/ipfs/Qmakme2N9gVDg9KPBN8EwGcYH4LM87WCmGsDCLZAUqJ5HH",
-    name: "Evening Alley",
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/tulum.JPG",
+    name: "Mayan Walled City",
+    photographer: "Jill Schweber",
+  },
+  {
+    city: "Bacalar",
+    country: "Mexico",
+    location: [18.676944, -88.395278],
+    tokenURI:
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/bacalar.json",
+    description: "Bacalar, Mexico",
+    image:
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/bacalar.JPG",
+    name: "Pueblo Mágico",
+    photographer: "Jill Schweber",
   },
   {
     city: "Vancouver",
     country: "BC",
     location: [49.2750752983092, -123.15402100302073],
     tokenURI:
-      "https://gateway.pinata.cloud/ipfs/QmdxYSVWofjT2hZQypGBB16bVLp2hiQhxLrmSXNqXmegsb/kitsBeach.json",
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/kitsBeach.json",
     description: "Vancouver, BC",
     image:
-      "https://gateway.pinata.cloud/ipfs/QmboYLZFLfVJMEgZQuJFVVrHycJLVLiR2SUtL6QofyJg4U/image4.jpeg",
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/image4.jpeg",
     name: "Kits Beach",
+    photographer: "Jill Schweber",
   },
   {
     city: "Vancouver",
     country: "BC",
     location: [49.331741955692536, -123.26365057440039],
     tokenURI:
-      "https://gateway.pinata.cloud/ipfs/QmdxYSVWofjT2hZQypGBB16bVLp2hiQhxLrmSXNqXmegsb/lighthousePark.json",
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/lighthousePark.json",
     description: "Vancouver, BC",
     image:
-      "https://gateway.pinata.cloud/ipfs/QmboYLZFLfVJMEgZQuJFVVrHycJLVLiR2SUtL6QofyJg4U/image6.jpeg",
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/image6.jpeg",
     name: "Lighthouse Park",
+    photographer: "Jill Schweber",
   },
   {
     city: "Tuscon",
     country: "Arizona",
     location: [32.221779, -111.14431],
     tokenURI:
-      "https://gateway.pinata.cloud/ipfs/QmdxYSVWofjT2hZQypGBB16bVLp2hiQhxLrmSXNqXmegsb/tuscon.json",
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/tuscon.json",
     description: "Tuscon, AZ",
     image:
-      "https://gateway.pinata.cloud/ipfs/QmboYLZFLfVJMEgZQuJFVVrHycJLVLiR2SUtL6QofyJg4U/image0.jpeg",
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/image0.jpeg",
     name: "Tuscon Cacti",
+    photographer: "Jill Schweber",
   },
   {
     city: "Joshua Tree",
     country: "California",
     location: [33.87572450273177, -115.85109529632798],
     tokenURI:
-      "https://gateway.pinata.cloud/ipfs/QmdxYSVWofjT2hZQypGBB16bVLp2hiQhxLrmSXNqXmegsb/joshuaTree.json",
+      "https://gateway.pinata.cloud/ipfs/QmeJ4fAsQUbftJYfmDSxRcYVhyF8VkqGvvZfrcVjupqMTb/joshuaTree.json",
     description: "Joshua Tree, CA",
     image:
-      "https://gateway.pinata.cloud/ipfs/QmboYLZFLfVJMEgZQuJFVVrHycJLVLiR2SUtL6QofyJg4U/image7.jpeg",
+      "https://gateway.pinata.cloud/ipfs/QmTeEu4XS3KYyjUar1AttWNREtgwvbWJiEnnftULEc1322/image7.jpeg",
     name: "Joshua Tree",
+    photographer: "Jill Schweber",
   },
 ];
